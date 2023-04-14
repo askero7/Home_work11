6ccf0860a0a7447e8c774c6f28665f1702ed4a83
Askatbek Zhunusaliuulu <askatbek.zhunusaliuulu.alt@gmail.com>
2023-04-14 18:40:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+            Uchburchtuk square = new  Uchburchtuk ();
+        square.a = 8;
+        square.b=8;
+        square.c=5;
+
+        square.area();
+
+    }
+}
---
+++ src/Uchburchtuk.java
@@ -0,0 +1,9 @@
+public class Uchburchtuk {
+    int a;
+    int b;
+    int c;
+
+    void area() {
+        System.out.println(a + b + c / 2);
+    }
+}
